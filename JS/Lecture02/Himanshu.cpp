88cbd6737af9e32ae7c1aef1866bdddf8d6da048
Himanshu Gupta <guptahimanshugupta@gmail.com>
2026-08-20 00:11:32 +0530

    JS Lecture 02

---
+++ JS/Lecture02/Himanshu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string code;
+    cout<<"Input the C++ code\n";
+    
+    getline(cin,code);
+
+    cout<<code<<endl;
+
+    return 0;
+}
